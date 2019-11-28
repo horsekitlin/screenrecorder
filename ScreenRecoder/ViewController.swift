31362c506b6c6f81e58dbfd3413bab808149f77b
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-11-28 17:23:26 +0800

    Initial Commit

---
+++ ScreenRecoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenRecoder
+//
+//  Created by 林暐秦 on 2019/11/28.
+//  Copyright © 2019 imappUITests. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
