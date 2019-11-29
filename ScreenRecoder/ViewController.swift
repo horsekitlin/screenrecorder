3350dfafebf468ebad99f30f012d813a02853f0b
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-11-29 13:36:34 +0800

    add update color

--- ScreenRecoder/ViewController.swift
+++ ScreenRecoder/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var colorPicker: UISegmentedControl!
+    @IBOutlet var colorDisplay: UIView!
+    @IBOutlet var micToggle: UISwitch!
+    @IBOutlet var recordButton: UIButton!
+
+    let recorder = RPScreenRecorder.shared()
+    private var isRecording = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        recordButton.layer.cornerRadius = 32.5
     }
-
-
+    
+    func viewReset() {
+        micToggle.isEnabled = true
+        statusLabel.text = "Ready to Record"
+        statusLabel.textColor = UIColor.black
+        recordButton.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func colors(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            colorDisplay.backgroundColor = UIColor.red
+        case 1:
+            colorDisplay.backgroundColor = UIColor.blue
+        case 2:
+            colorDisplay.backgroundColor = UIColor.orange
+        case 3:
+            colorDisplay.backgroundColor = UIColor.green
+        default:
+            colorDisplay.backgroundColor = UIColor.red
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
